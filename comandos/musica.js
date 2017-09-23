90a33cc541675e01699b9e222c1831a9fac3f077
NecaX <pablo_1997_olivas@hotmail.es>
2017-09-23 20:51:00 +0200

    musica cambio de nombre

--- comandos/musica.js
+++ comandos/musica.js
@@ -4,14 +4,14 @@ exports.run = (client, message, args) => {
 		  message.member.voiceChannel.join()
 			.then(connection => { 
 				const fs = require('fs');
-				//const stream = fs.createReadStream('./musica/coches de choque.mp3');
-				//const dispatcher = connection.playStream(stream);
-				//dispatcher.setVolume(0.15);
-				var path = './musica'
-				fs.readdir(path, function (err, files) { //Leemos los ficheros de la carpeta con ubicacion en path
-					if (err) return console.log(err);
-					message.channel.send(files).catch(console.error); //Escribe todo el array files o files[x]
-				});
+				const stream = fs.createReadStream('./musica/coches de choque.mp3');
+				const dispatcher = connection.playStream(stream);
+				dispatcher.setVolume(0.15);
+				// var path = './musica'
+				// fs.readdir(path, function (err, files) { //Leemos los ficheros de la carpeta con ubicacion en path
+					// if (err) return console.log(err);
+					// message.channel.send(files).catch(console.error); //Escribe todo el array files o files[x]
+				// });
 			})
 			.catch(console.log);
 		} else {
